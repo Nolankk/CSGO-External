02cc30b727650eea0c9f008cee688986696177e5
Nolan <nolanpolak@gmail.com>
2020-04-21 21:12:39 -0500

    Fixed Glow ESP
    No flickering, only shows enemys.

--- CSGO External/Features/Visuals.cs
+++ CSGO External/Features/Visuals.cs
@@ -21,22 +21,20 @@ namespace CSGO_External.Render
         {
             while (true)
             {
-                for (var i = 0; i < 64; i++)
+                for (var i = 0; i < 32; i++)
                 {
                     int curEnt = Program.Mem.ReadInt(Offsets.baseClient + Offsets.dwEntityList + i * 0x10);
-                    int entTeam = Program.Mem.ReadInt(curEnt + 0xF0);
-                    int dormantt = Program.Mem.ReadInt(curEnt + 0xED);
-
-                    if (curEnt == 0)
-                       continue;
+                    int curTeam = Program.Mem.ReadInt(curEnt + Offsets.m_iTeamNum);
+                    int curDormant = Program.Mem.ReadInt(curEnt + 0xED);
+                    int curGlowIndex = Program.Mem.ReadInt(curEnt + Offsets.m_iGlowIndex);
 
                     if (curEnt == LocalPlayer.localBase)
                         continue;
 
-                    if (LocalPlayer.localTeam == entTeam)
+                    if (LocalPlayer.localTeam == curTeam)
                         continue;
 
-                    if (dormantt == 1)
+                    if (curDormant == 1 || curTeam == 0)
                        continue;
 
                     GlowStruct Enemy = new GlowStruct()
@@ -46,11 +44,11 @@ namespace CSGO_External.Render
                         b = 0,
                     };
 
-                    Program.Mem.WriteFloat(LocalPlayer.glowBase + (glowIndex * 0x38 + 0x4), Enemy.r);
-                    Program.Mem.WriteFloat(LocalPlayer.glowBase + (glowIndex * 0x38 + 0x8), Enemy.g);
-                    Program.Mem.WriteFloat(LocalPlayer.glowBase + (glowIndex * 0x38 + 0xC), Enemy.b);
-                    Program.Mem.WriteFloat(LocalPlayer.glowBase + (glowIndex * 0x38 + 0x10), 1.0f);
-                    Program.Mem.WriteByte(LocalPlayer.glowBase + (glowIndex * 0x38 + 0x24), 1);
+                    Program.Mem.WriteFloat(LocalPlayer.glowBase + (curGlowIndex * 0x38 + 0x4), Enemy.r);
+                    Program.Mem.WriteFloat(LocalPlayer.glowBase + (curGlowIndex * 0x38 + 0x8), Enemy.g);
+                    Program.Mem.WriteFloat(LocalPlayer.glowBase + (curGlowIndex * 0x38 + 0xC), Enemy.b);
+                    Program.Mem.WriteFloat(LocalPlayer.glowBase + (curGlowIndex * 0x38 + 0x10), 1.0f);
+                    Program.Mem.WriteByte(LocalPlayer.glowBase + (curGlowIndex * 0x38 + 0x24), 1);
                 }
                 Thread.Sleep(1);
             }
--- CSGO External/Utils/Read.cs
+++ CSGO External/Utils/Read.cs
@@ -22,22 +22,7 @@ namespace CSGO_External.Utils
                 LocalPlayer.localTeam = Program.Mem.ReadInt(LocalPlayer.localBase + Offsets.m_iTeamNum);
                 LocalPlayer.forceJump = Offsets.baseClient + Offsets.dwForceJump;
                 LocalPlayer.flags = Program.Mem.ReadInt(LocalPlayer.localBase + Offsets.m_fFlags);
-
-                for (var i = 0; i < 64; i++)
-                {
-                    Entity.entityBase = Program.Mem.ReadInt(Offsets.baseClient + Offsets.dwEntityList + i * 0x10);
-                    if (entityBase > 0)
-                    {
-                        Entity.dormant = Program.Mem.ReadInt(entityBase + 0xED);
-                        Entity.glowIndex = Program.Mem.ReadInt(entityBase + Offsets.m_iGlowIndex);
-                        Entity.m_VecOrigin = Program.Mem.ReadMemory<Vector3D>(entityBase + Offsets.m_vecOrigin);
-                        Entity.m_VecMin = Program.Mem.ReadMemory<Vector3D>(entityBase + Offsets.m_vecMins);
-                        Entity.m_VecMax = Program.Mem.ReadMemory<Vector3D>(entityBase + Offsets.m_vecMaxs);
-                        Entity.entityTeam = Program.Mem.ReadInt(entityBase + Offsets.m_iTeamNum);
-                        LocalPlayer.Arrays.ViewMatrix = Program.Mem.ReadMatrix<float>(Offsets.baseClient + Offsets.dwViewMatrix, 16);
-                    }
-                }
-               // Thread.Sleep(10);
+                //Thread.Sleep(10);
             }
         }   
     }
